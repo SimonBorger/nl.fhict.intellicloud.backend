432b0dcd0a14052651ecad839330028bdf1f43ab
SimonBorger <simonborger@gmail.com>
2013-12-04 21:50:02 +0100

    Added WordStore
    WordStore is a WCF Service that is used to resolve words into their
    actual types and languages.

---
+++ WordStore/WordStore.Business/Conversion/IConversion.cs
@@ -0,0 +1,25 @@
+﻿using IntelliCloud.WordStore.Common.Word;
+using IntelliCloud.WordStore.Data.Database.Model;
+using System.Collections.Generic;
+using WordObject = IntelliCloud.WordStore.Common.Word.Word;
+
+namespace IntelliCloud.WordStore.Business.Conversion
+{
+    /// <summary>
+    /// An interface for a conversion object providing all functionality to convert objects.
+    /// </summary>
+    public interface IConversion
+    {
+        #region Methods
+
+        WordObject ToWord(WordEntity wordEntity);
+
+        KeywordEntity ToKeywordEntity(string keyword, IEnumerable<WordObject> matchingWords);
+
+        WordType ToWordType(string wordType);
+
+        Language ToLanguage(string language);
+
+        #endregion Methods
+    }
+}
---
+++ WordStore/WordStore.Service.IntegrationTest/WordServiceTest.cs
@@ -0,0 +1,56 @@
+﻿using IntelliCloud.WordStore.Common.Word;
+using IntelliCloud.WordStore.Data.Database.Factory;
+using IntelliCloud.WordStore.Data.Database.Model;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Timers;
+
+namespace IntelliCloud.WordStore.Service.IntegrationTest
+{
+    [TestClass]
+    public class WordServiceTest
+    {
+        #region Fields
+
+        private IWordService service;
+
+        #endregion Fields
+
+        #region Methods
+
+        [TestInitialize]
+        public void Initialize()
+        {
+            this.service = new WordService();
+        }
+
+        #region Tests
+
+        [TestMethod]
+        [TestCategory("IntelliCloud.Service.IntegrationTest")]
+        public void Test()
+        {
+            Stopwatch stopwatch = new Stopwatch();
+            stopwatch.Start();
+            var words1 = this.service.ResolveWord("show");
+            stopwatch.Stop();
+            Debug.WriteLine(string.Format("Found: {0} words in {1}", words1.Count(), stopwatch.ElapsedMilliseconds));
+
+            stopwatch.Reset();
+
+            stopwatch.Start();
+            var words2 = this.service.ResolveWord("show");
+            stopwatch.Stop();
+            Debug.WriteLine(string.Format("Found: {0} words in {1}", words2.Count(), stopwatch.ElapsedMilliseconds));
+        }
+
+        #endregion Tests
+
+        #endregion Methods
+    }
+}
